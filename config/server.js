695a3507f51294305bc94f0aa5d99d5fc24b9fae
Rui Cui <cruilnsy@gmail.com>
2021-02-13 16:53:09 -0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'db4738bcd7e2cea019b7e150ff379e50'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8e588e2f-6f77-4129-b6b3-7d68c265d226'
+};
